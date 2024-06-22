7794bfd5884d6139b694d7f6c757489298d94094
Muntasir Rifat <rifat2002m@gmail.com>
2024-06-22 20:53:27 +0600

    items

--- src/Component/Items/Items.jsx
+++ src/Component/Items/Items.jsx
@@ -1,8 +1,39 @@
+import { useLoaderData } from "react-router-dom";
+import Card from "../Home/Card";
+import { useContext, useState } from "react";
+import { AuthContext } from "../Auth/AuthProvider";
 
 const Items = () => {
+    const user = useContext(AuthContext);
+    const [dataLength, setDataLength] = useState(6);
+
+    let foods = [];
+    try {
+        foods = useLoaderData();
+    } 
+    catch (error) {
+        console.error("Failed to load data:", error);
+    }
+
+    const validFoods = Array.isArray(foods) ? foods : [];
     return (
         <div>
-            item
+             <div className="col-span-2 m-4">
+                    {/* Middle Page */}
+                    <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-4'>
+                    {
+                            validFoods.slice(0, dataLength).map(food => (
+                                <Card key={food.id} food={food}></Card>
+                            ))
+                        }
+                    </div>
+                    <button
+                        className="btn btn-error mt-6 font-bold mx-auto justify-center card px-12 text-xl"
+                        onClick={() => setDataLength(validFoods.length)}
+                    >
+                        Show All
+                    </button>
+                    </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -39,6 +39,7 @@ const router = createBrowserRouter([
       }, 
       {
         path:'/item',
+        loader: ()=>fetch('/public/items.json'),
         element:<Items></Items>
       },
       {
