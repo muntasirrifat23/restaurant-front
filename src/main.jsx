21081b391d922c322b1510c72171a517652a0bed
Muntasir Rifat <rifat2002m@gmail.com>
2024-07-30 21:38:07 +0600

    private admin main

--- src/main.jsx
+++ src/main.jsx
@@ -90,7 +90,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/addItems",
-        element: <AddItems></AddItems>,
+        element: <AdminPrivate><AddItems></AddItems></AdminPrivate>,
       },
     ],
   },
